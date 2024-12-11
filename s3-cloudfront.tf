51536d1ce8000b1b614f9a706983b4556198cf1a
Roland Li <r@mmsg.com>
2024-12-10 22:34:05 -0700

    fine tune block public policy

--- s3-cloudfront.tf
+++ s3-cloudfront.tf
@@ -27,6 +27,12 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  block_public_acls   = false
+  block_public_policy = false
+}
+
 resource "aws_s3_bucket_acl" "s3_bucket_acl" {
   bucket = aws_s3_bucket.s3_bucket.id
   acl    = "private"
